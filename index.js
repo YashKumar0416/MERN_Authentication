5fc96234298e2fed5c48814ca3672ed075e32adf
YashKumar0416 <yash16y03y2001y@gmail.com>
2023-02-11 14:46:51 +0530

    u

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ const PORT = process.env.PORT || 8000;
 // })
 
 //STATIC FILES
-app.use(express.static(path.join(__dirname, './client/build')))
+app.use(express.static(path.join(__dirname, '/client/build')))
 app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, './client/build/index.html'))
+    res.sendFile(path.join(__dirname, '/client/build/index.html'))
 });
 
 app.use(cors());
